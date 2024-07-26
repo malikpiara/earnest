4fb5375653c8532f582502c5d702f0594fef5bcd
Malik Piara <malik@hey.com>
2024-07-26 13:51:07 +0200

    Changed color scheme.

--- components/card.js
+++ components/card.js
@@ -31,9 +31,9 @@ export const QuestionCard = ({ question, onSwipeComplete }) => {
             dragConstraints={{ top: -50, bottom: 50, left: 0, right: 0 }}
             whileTap={{ scale: 1.02 }}
             onDragEnd={handleDragEnd}
-            className="flex flex-col md:w-72 md:h-96 h-[85dvh] bg-cyan-950 rounded-2xl overflow-hidden inset-0 m-auto" >
-                <div className="flex text-3xl md:text-xl text-cyan-700 relative top-20 w-9/12 md:w-11/12 md:m-5 ml-10">{question.text}</div>
-                <div className='flex rounded-full w-72 h-72 bg-cyan-800 opacity-40 blur-2xl relative top-40'/>
+            className="flex flex-col md:w-72 md:h-96 h-[85dvh] bg-[#BEB3FF] rounded-2xl overflow-hidden inset-0 m-auto" >
+                <div className="flex text-3xl md:text-xl text-[#5f52ad] relative top-20 w-9/12 md:w-11/12 md:m-5 ml-10 font-medium">{question.text}</div>
+                <div className='flex rounded-full w-72 h-72 bg-[#9181ec] blur-2xl relative top-40'/>
         </motion.div>
     )
 }
